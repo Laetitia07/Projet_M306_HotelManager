58063bcba06e9532be6ad98e3638aee91bda1a05
merlia774 <elise.lvq@eduge.ch>
2022-02-21 10:25:25 +0100

    squelette réservation

---
+++ View/reservation.php
@@ -0,0 +1,17 @@
+<?php
+/*
+*Nom Prénom : Leveque Elise
+*Date : 28.03
+*Description : Relie le client à la chambre à travers une réservation 
+*/
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Réservation</title>
+</head>
+<body>
+    
+</body>
+</html>
