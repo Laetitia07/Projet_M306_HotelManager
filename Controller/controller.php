5ef209b78e2b98b7ced671a97aa88a47b0463bed
Laetitia07 <60383861+Laetitia07@users.noreply.github.com>
2022-03-14 10:51:13 +0100

    test4

--- Controller/controller.php
+++ Controller/controller.php
@@ -1,60 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
 require_once "../Model/model.php";//testPull
 function controlInputRoom($NumChambre, $TypeChambre, $TelChambre){
@@ -75,6 +18,27 @@ function controlInputRoom($NumChambre, $TypeChambre, $TelChambre){
         }
     } else {
         $error = "Veuillez remplir tous les champs correctement !";
+    }
+    return $error;
+}
+function addqqch($NumChambre, $TypeChambre, $TelChambre){
+    
+    $error = "";
+    if (isset(add) && !empty(ddd) && isset(zzz) && !empty(hhtzjh) && isset(hf) && !empty($TelChambre)) {
+        $num = filter_var(fdh, FILTER_SANITIZE_STRING);
+        $type = filter_var(fgjn, FILTER_SANITIZE_STRING);
+        $telephone = filter_var($TelChambre, FILTER_SANITIZE_STRING);
+       
+        if (checkRoom($num)) {     
+
+            addRoom($num, $type, fg);          
+            //$_SESSION['tel'] = $Telephone;
+            //$_SESSION['fgh'] = gfdh;    
+        } else {
+            $error = "Cette chambre existe déjà";
+        }
+    } else {
+        $egfhhffg = "rectement !";
     }
     return $error;
 }
